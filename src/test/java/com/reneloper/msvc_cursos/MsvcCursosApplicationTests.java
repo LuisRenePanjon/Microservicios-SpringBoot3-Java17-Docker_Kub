ca0a52fccb31c8d76317ce2f9118a33f8461c831
Luis René Panjón <luchorene97@gmail.com>
2023-01-18 11:00:00 -0500

    Inicializar proyecto de spring boot v3 con java 17

---
+++ src/test/java/com/reneloper/msvc_cursos/MsvcCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reneloper.msvc_cursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcCursosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
